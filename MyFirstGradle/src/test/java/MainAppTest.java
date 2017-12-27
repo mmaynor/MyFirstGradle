eceb7e0f947aea365e1e190a8f9bdf27c159d33b
mmaynor <34344352+mmaynor@users.noreply.github.com>
2017-12-27 16:17:09 -0500

    My first gradle project

---
+++ MyFirstGradle/src/test/java/MainAppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainAppTest {
+
+   @Test
+   public void testSayHello() {
+      MainApp app = new MainApp();
+      assertNotNull("Success", app.sayHello());
+   }
+}
